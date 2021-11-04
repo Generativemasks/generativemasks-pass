0e9cc3751073c35560466bb152bedb4aa37520e4
wild-mouse <wildmouse0813@gmail.com>
2021-11-04 21:56:28 +0900

    feat: add tokenURI

--- contracts/derivatives/GMDerivative.sol
+++ contracts/derivatives/GMDerivative.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "../interfaces/IN.sol";
 import "../interfaces/iGM.sol";
 import "../core/GMPassCore.sol";
@@ -10,6 +11,8 @@ import "../core/GMPassCore.sol";
  * @title GenerativemasksDerivative
  */
 contract GenerativemasksDerivative is GMPassCore {
+    
+    using Strings for uint256;
 
     address public derivedFrom;
     string private __baseURI;
@@ -22,16 +25,29 @@ contract GenerativemasksDerivative is GMPassCore {
         address _derivedFrom
     )
     GMPassCore(name, symbol, IN(_generativemasks), true, 10000, 10000, 0, 0)
-    // GMPassCore(name, symbol, iGM(_generativemasks), true, 10000, 10000, 0, 0)
+        // GMPassCore(name, symbol, iGM(_generativemasks), true, 10000, 10000, 0, 0)
     {
         __baseURI = baseURI;
         derivedFrom = _derivedFrom;
     }
-    
+
     function _baseURI() internal override view virtual returns (string memory) {
         return __baseURI;
     }
-    
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI = _baseURI();
+
+        if (MAX_GM_TOKEN_ID < tokenId) {
+            return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        }
+
+        uint256 maskNumber = (tokenId + METADATA_INDEX) % GM_SUPPLY_AMOUNT;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, maskNumber.toString())) : "";
+    }
+
     function updateBaseURI(string calldata newBaseURI) external onlyOwner {
         __baseURI = newBaseURI;
     }
