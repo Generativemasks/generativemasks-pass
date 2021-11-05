b70eefa4eff2c3ec0e47e6bd07978c884542e67b
wild-mouse <wildmouse0813@gmail.com>
2021-11-05 14:06:07 +0900

    feat: add gm derivative for testing

--- test/fixtures/index.ts
+++ test/fixtures/index.ts
@@ -1,11 +1,12 @@
 import { deployments, getNamedAccounts, getUnnamedAccounts } from "hardhat";
-import { ERC721Mock } from "../../typechain";
+import { ERC721Mock, GenerativemasksDerivative } from "../../typechain";
 import { GMPass } from "../../typechain";
 import { ETH } from "../../utils/utils";
 import { setupUser, setupUsers } from "./users";
 
 export interface Contracts {
   NFT: ERC721Mock;
+  GMDerivative: GenerativemasksDerivative;
   MockGMDerivative: GMPass;
   MockGMDerivativeRestricted: GMPass;
   MockGMDerivativeWithAllowance: GMPass;
@@ -22,6 +23,15 @@ export const setupIntegration = deployments.createFixture(async ({ ethers }) => 
   const nftContractFactory = await ethers.getContractFactory("ERC721Mock");
   const nftContract = (await nftContractFactory.deploy()) as ERC721Mock;
   const nftAddress = nftContract.address;
+
+  const GMPassFactory = await ethers.getContractFactory("GenerativemasksDerivative");
+  const GMDerivative = (await GMPassFactory.deploy(
+    "https://example.com/",
+    "GMD",
+    "GMD",
+    nftAddress,
+    nftAddress,
+  )) as GenerativemasksDerivative;
 
   const mockGMPassFactory = await ethers.getContractFactory("MockGMPass");
   const mockGMDerivative = (await mockGMPassFactory.deploy("GMD", "GMD", nftAddress, false, 8888, 0, 0, 0)) as GMPass;
@@ -57,6 +67,7 @@ export const setupIntegration = deployments.createFixture(async ({ ethers }) => 
   )) as GMPass;
 
   const contracts: Contracts = {
+    GMDerivative,
     MockGMDerivative: mockGMDerivative,
     MockGMDerivativeRestricted: mockGMDerivativeRestricted,
     MockGMDerivativeWithAllowance: mockGMDerivativeWithAllowance,
