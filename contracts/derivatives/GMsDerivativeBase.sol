028dcef7651a36df5f5ba87cda77d7d4d4dbfd17
wild-mouse <wildmouse0813@gmail.com>
2021-11-08 15:48:45 +0900

    feat: add derivative base contract

---
+++ contracts/derivatives/GMsDerivativeBase.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "../core/GMsPassCore.sol";
+
+contract GMsDerivativeBase is GMsPassCore {
+
+    using Strings for uint256;
+
+    address public derivedFrom;
+    string private __baseURI;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseURI,
+        address generativemasks,
+        address _derivedFrom
+    )
+    GMsPassCore(
+        name,
+        symbol,
+        IERC721(generativemasks),
+        true,
+        10000,
+        10000,
+        0, 
+        0
+    )
+    {
+        __baseURI = baseURI;
+        derivedFrom = _derivedFrom;
+    }
+
+    function _baseURI() internal override view virtual returns (string memory) {
+        return __baseURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI = _baseURI();
+        if (MAX_GMs_TOKEN_ID < tokenId) {
+            return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        }
+
+        uint256 maskNumber = (tokenId + METADATA_INDEX) % GMS_SUPPLY_AMOUNT;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, maskNumber.toString())) : "";
+    }
+
+    function updateBaseURI(string calldata newBaseURI) external onlyOwner {
+        __baseURI = newBaseURI;
+    }
+}
